83fbc2c7a3d1311cacf0a5e1bc18b450ca0d6c95
hansung <hansung080@hanmail.net>
2024-01-26 21:01:17 +0900

    modified bool from #define to typedef

--- c-project/src/types.h
+++ c-project/src/types.h
@@ -1,8 +1,13 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
-#define bool  unsigned char
 #define false 0
 #define true  1
+
+typedef unsigned char  bool;
+typedef unsigned char  uchar;
+typedef unsigned short ushort;
+typedef unsigned int   uint;
+typedef unsigned long  ulong;
 
 #endif // __TYPES_H__
